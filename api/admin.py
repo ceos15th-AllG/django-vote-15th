491fab090d96ca6382a550107f250c746c2a839f
yesjjin99 <yesjjin99@naver.com>
2022-05-26 07:34:33 +0900

    Update admin settings

--- api/admin.py
+++ api/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(User)
-admin.site.register(Candidate)
-admin.site.register(Vote)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+	list_display = ['id', 'username', 'email', 'password', 'is_voted']
+	list_display_links = ['id', 'username']
+
+
+@admin.register(Candidate)
+class CandidateAdmin(admin.ModelAdmin):
+	list_display = ['id', 'name', 'count']
+	list_display_links = ['id', 'name']
+
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+	list_display = ['id', 'candidate', 'user', 'created_at', 'updated_at']
+	list_display_links = ['id']
