213b38a2563f5e2b0571553c5b086f860e4b8e19
yesjjin99 <yesjjin99@naver.com>
2022-05-26 02:43:45 +0900

    Fix models
    생성일자, 수정일자 필드 추가 및 Vote 모델의 필드 이름 변경

--- api/models.py
+++ api/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class DateTime(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
 class User(AbstractUser):
     email = models.EmailField(max_length=100, unique=True)
     is_voted = models.BooleanField(default=False)
@@ -12,6 +20,6 @@ class Candidate(models.Model):
     count = models.PositiveIntegerField(default=0)
 
 
-class Vote(models.Model):
-    candidate_id = models.ForeignKey(Candidate, on_delete=models.CASCADE, related_name='votes')
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name='votes')
+class Vote(DateTime):
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE, related_name='votes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='votes')
