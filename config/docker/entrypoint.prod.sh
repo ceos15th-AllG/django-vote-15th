a5a20c9caf65aea3e20070481f3f3ddc9322f27d
yesjjin99 <yesjjin99@naver.com>
2022-06-15 19:46:32 +0900

    Remove create superuser logic

--- config/docker/entrypoint.prod.sh
+++ config/docker/entrypoint.prod.sh
@@ -6,13 +6,4 @@ python manage.py collectstatic --no-input
 echo "Migrate"
 python manage.py migrate
 
-echo "Create superuser"
-if [ "$DJANGO_SUPERUSER_USERNAME" ]
-then
-    python manage.py createsuperuser \
-        --noinput \
-        --username $DJANGO_SUPERUSER_USERNAME \
-        --email $DJANGO_SUPERUSER_USERNAME
-fi
-
 exec "$@"
