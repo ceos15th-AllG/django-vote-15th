6d82ab4ed19dfe1d4b9d3108d12688bfeb9f951a
doyeoo <ehdusla013@gmail.com>
2022-06-17 19:14:50 +0900

    Fix conflicts

--- api/views.py
+++ api/views.py
@@ -25,20 +25,12 @@ class SignUpView(views.APIView):
             refresh = str(token)
             access = str(token.access_token)
 
-<<<<<<< HEAD
             return JsonResponse({
-=======
-            # request.session['access_token'] = access
-            # request.session['refresh_token'] = refresh
-
-            response = JsonResponse({
->>>>>>> 47aaba645ba38589c3ca427d20224294443c1264
                 'message': 'Signup Success',
                 'user': user.username,
                 'access': access,
                 'refresh': refresh,
             }, status=HTTP_201_CREATED)
-
         else:
             return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
@@ -54,14 +46,7 @@ class LoginView(views.APIView):
             refresh = serializer.validated_data['refresh']
             access = serializer.validated_data['access']
 
-<<<<<<< HEAD
             return JsonResponse({
-=======
-            # request.session['access_token'] = access
-            # request.session['refresh_token'] = refresh
-
-            response = JsonResponse({
->>>>>>> 47aaba645ba38589c3ca427d20224294443c1264
                 'message': 'Login Success',
                 'user': user.username,
                 'access': access,
@@ -73,39 +58,6 @@ class LoginView(views.APIView):
             return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
 
 
-<<<<<<< HEAD
-=======
-class LogoutView(views.APIView):
-    def post(self, request):
-        # del request.session['access_token']
-        # del request.session['refresh_token']
-
-        response = JsonResponse({
-            'message': 'Logout Success',
-        }, status=HTTP_200_OK)
-
-        response.delete_cookie('access_token')
-        response.delete_cookie('refresh_token')
-
-        return response
-
-
-class VotePermission(permissions.BasePermission):
-    def has_permission(self, request, view):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        else:
-            if request.COOKIES.get('access_token'):
-                access = request.COOKIE['access_token']
-                payload = jwt.decode(access, env('DJANGO_SECRET_KEY'), algorithms=['HS256'])
-                user = get_object_or_404(User, pk=payload['user_id'])
-                request.user = user
-                return True
-            else:
-                return False
-
-
->>>>>>> 47aaba645ba38589c3ca427d20224294443c1264
 class VoteListView(views.APIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
@@ -117,7 +69,10 @@ class VoteListView(views.APIView):
 
     def post(self, request):
         user = get_object_or_404(User, pk=request.user.id)
-        candidate = get_object_or_404(Candidate, pk=request.query_params.get('candidate'))  # querystring
+        candidate_id = request.query_params.get('candidate')
+        if (not candidate_id):
+            return JsonResponse({'message': 'Invalid Query Parameters'}, status=HTTP_400_BAD_REQUEST)
+        candidate = get_object_or_404(Candidate, pk=candidate_id)
 
         data = {'candidate': candidate.id, 'user': user.id}
         vote_serializer = VoteSerializer(data=data)
