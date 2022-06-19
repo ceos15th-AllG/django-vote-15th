de9cfbd0b4c4ea10c6b3fdc00dfa1f9fd56ced36
gkim <kimkm0419@naver.com>
2022-06-19 22:36:24 +0900

    Feat : Add Token related views to url.py

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from api import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('signup/', views.Signup.as_view()),
     path('login/', views.LoginView.as_view()),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
     path('vote/', views.Vote.as_view()),
 ]
