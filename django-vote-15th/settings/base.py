406ac7dad290ca9ec3486c98ba7726224f466826
gkim <kimkm0419@naver.com>
2022-06-21 11:57:16 +0900

    Feat : Add django-cors setting

--- django-vote-15th/settings/base.py
+++ django-vote-15th/settings/base.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders'
 ]
 
 REST_FRAMEWORK = {
@@ -70,7 +71,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000','http://localhost:3000']
+
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'django-vote-15th.urls'
 
