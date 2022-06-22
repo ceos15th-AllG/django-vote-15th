5d101f5ca82cd11f4808518fff516eebd2035618
woozoo_zinc <your_zinc@ewhain.net>
2022-06-23 01:19:23 +0900

    test : test api

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('test/', TestAPIView.as_view()),
     path('/signup', SignUpAPIView.as_view()),
     path('/login', LoginAPIView.as_view()),
-    path('/test', TestAPIView.as_view()),
 ]
