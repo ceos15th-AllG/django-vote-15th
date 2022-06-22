f6fddfb3eb135f1b89c5d5271ff58aa4194a84d9
yesjjin99 <yesjjin99@naver.com>
2022-06-23 01:19:45 +0900

    Add cookie secure, samesite option

--- django-vote-15th/settings/base.py
+++ django-vote-15th/settings/base.py
@@ -149,14 +149,8 @@ CORS_ORIGIN_WHITELIST = (
 )
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_HEADERS = (
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-)
+# cookie
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
