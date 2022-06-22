1f654220a57cfa08b026c18230cf29aa3b9494cc
woozoo_zinc <77188666+yourzinc@users.noreply.github.com>
2022-06-23 01:26:46 +0900

    update : url.py
    '/test' -> 'test'

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path('/signup', SignUpAPIView.as_view()),
     path('/login', LoginAPIView.as_view()),
-    path('/test', TestAPIView.as_view()),
+    path('test', TestAPIView.as_view()),
 ]
