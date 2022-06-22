4f64b709f6a00a8efd9cbe2e827cbb17e213455e
doyeoo <ehdusla013@gmail.com>
2022-06-22 22:36:17 +0900

    Add cors header setting

--- django-vote-15th/settings/base.py
+++ django-vote-15th/settings/base.py
@@ -148,3 +148,15 @@ CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:8000',
 )
 CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
