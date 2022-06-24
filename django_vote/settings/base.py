4e0c24add52ee87d2b292d74f98f9c511595bae2
김민준 <sanbonai06@naver.com>
2022-06-25 02:19:19 +0900

    Token Test

--- django_vote/settings/base.py
+++ django_vote/settings/base.py
@@ -142,8 +142,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=2),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
+    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=50),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
